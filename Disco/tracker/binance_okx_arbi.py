842771d44333ee3dc55d0fc071d59d34abb9aa52
yun1ongm <mustmyles@gmail.com>
2024-03-03 20:27:25 +0800

    update arbitrager

--- Disco/tracker/binance_okx_arbi.py
+++ Disco/tracker/binance_okx_arbi.py
@@ -1,5 +1,6 @@
 import logging
 import time
+from retry import retry
 
 import okx.MarketData as MarketData
 from binance.um_futures import UMFutures
@@ -14,28 +15,9 @@ class BnOkxArbi:
     This class is used to track arbitrage opportunities between Binance and Okx
     """
     arbitrage_name = "binance_okx_arbitrage"
-    symbols_list = ["BTCUSD", "ETHUSD", "SOLUSD", "WLDUSD", "ORDIUSD", 
-                    "DOGEUSD", "XRPUSD", "PEOPLEUSD"]
-    binance_symbol_mapping = {
-        "BTCUSD": "BTCUSDT",
-        "ETHUSD": "ETHUSDT",
-        "SOLUSD": "SOLUSDT",  
-        "WLDUSD": "WLDUSDT",
-        "ORDIUSD": "ORDIUSDT",
-        "DOGEUSD": "DOGEUSDT",
-        "XRPUSD": "XRPUSDT",
-        "PEOPLEUSD": "PEOPLEUSDT",
-    }
-    okx_symbol_mapping = {
-        "BTCUSD": "BTC-USDT-SWAP",
-        "ETHUSD": "ETH-USDT-SWAP", 
-        "SOLUSD": "SOL-USDT-SWAP",
-        "WLDUSD": "WLD-USDT-SWAP",
-        "ORDIUSD": "ORDI-USDT-SWAP",
-        "DOGEUSD": "DOGE-USDT-SWAP",
-        "XRPUSD": "XRP-USDT-SWAP",
-        "PEOPLEUSD": "PEOPLE-USDT-SWAP"
-    }
+    symbols_list = ["BTCUSD", "ETHUSD", "SOLUSD", "BCHUSD", "ORDIUSD", 
+                    "DOGEUSD", "XRPUSD"]
+
     bin_comm = 0.0005
     okx_comm = 0.0005
 
@@ -44,7 +26,20 @@ class BnOkxArbi:
     def __init__(self):
         self.binance = UMFutures()
         self.okx =  MarketData.MarketAPI(flag="0",debug=False)
+        self.binance_symbol_mapping = self._generate_mapping(self.symbols_list, 'binance')
+        self.okx_symbol_mapping = self._generate_mapping(self.symbols_list, 'okx')
 
+    def _generate_mapping(self, symbols_list: list, exchange: str):
+        mapping = {}
+        for symbol in symbols_list:
+            base = symbol.split('USD')[0]
+            if exchange == 'binance':
+                mapping[symbol] = base + 'USDT'
+            elif exchange == 'okx':
+                mapping[symbol] = base + '-USDT-SWAP'
+        return mapping
+
+    @retry(tries=3, delay=1)
     def fetch_tickers(self) -> dict:
         try:
             tickers = {}
@@ -64,41 +59,51 @@ class BnOkxArbi:
         for symbol, ticker in tickers.items():
             okx_ticker = ticker['okx_ticker']['data'][0]
             binance_ticker = ticker['bin_ticker']
-            order_book[symbol] = {
-                'okx_bid': float(okx_ticker['bids'][0][0]),
-                'okx_bid_size': float(okx_ticker['bids'][0][1]) / 100, 
-                'okx_ask': float(okx_ticker['asks'][0][0]),
-                'okx_ask_size': float(okx_ticker['asks'][0][1]) / 100,
-                'bin_bid': float(binance_ticker['bidPrice']),
-                'bin_bid_size' : float(binance_ticker['bidQty']),
-                'bin_ask': float(binance_ticker['askPrice']),
-                'bin_ask_size' : float(binance_ticker['askQty'])
-            }
+
+            okx_bid = float(okx_ticker['bids'][0][0])
+            okx_bid_size = float(okx_ticker['bids'][0][1]) / 100
+            okx_ask = float(okx_ticker['asks'][0][0])
+            okx_ask_size = float(okx_ticker['asks'][0][1]) / 100
+            bin_bid = float(binance_ticker['bidPrice'])
+            bin_bid_size = float(binance_ticker['bidQty'])
+            bin_ask = float(binance_ticker['askPrice'])
+            bin_ask_size = float(binance_ticker['askQty'])
+
+            if okx_bid != 0 and okx_ask != 0 and bin_bid != 0 and bin_ask != 0:
+                order_book[symbol] = {
+                    'okx_bid': okx_bid,
+                    'okx_bid_size': okx_bid_size,
+                    'okx_ask': okx_ask,
+                    'okx_ask_size': okx_ask_size,
+                    'bin_bid': bin_bid,
+                    'bin_bid_size' : bin_bid_size,
+                    'bin_ask': bin_ask,
+                    'bin_ask_size' : bin_ask_size
+                }
         
         return order_book
     
     def is_arbi_trade(self, order_book: dict) -> bool:
         for symbol, book in order_book.items():
-            price = book['okx_bid']
-            okx_bid_offset = book['okx_bid']*(1-self.okx_comm*2)
-            okx_ask_offset = book['okx_ask']* (1+self.okx_comm*2)
-            bin_bid_offset= book['bin_bid']*(1-self.bin_comm*2)
-            bin_ask_offset = book['bin_ask']* (1+self.bin_comm*2)
-            bid_gap = round((okx_bid_offset - bin_bid_offset) *100/price, 2)
-            self.logger.info(f'{symbol} bid gap: {bid_gap}%')
+            offset = round(book['bin_bid'] * self.okx_comm*2,4)
+            positive_gap = round((book['okx_bid'] - book['bin_ask']),4)
+            negative_gap = round((book['bin_bid'] - book['okx_ask']),4)
+            self.logger.warning(f'{symbol}:positive gap: {positive_gap}, negative gap: {negative_gap}, offset: {offset}')
 
-            if okx_bid_offset > bin_ask_offset:
-                gap_price = round((okx_bid_offset - bin_ask_offset) *100 / price, 2)
-                self.logger.warning(f'Buy {symbol} on Binance, Sell {symbol} on Okx, gap: {gap_price}%')
+            if positive_gap > offset:
+                okx_bid = book['okx_bid']
+                bin_ask = book['bin_ask']
+                self.logger.warning(f'Buy {symbol} on Binance at {bin_ask}, Sell {symbol} on Okx at {okx_bid}')
                 okx_bid_size = book['okx_bid_size']
                 bin_ask_size = book['bin_ask_size']
                 trading_lot = min(okx_bid_size, bin_ask_size)
                 self.logger.warning(f'size: {trading_lot}')
                 return True
             
-            elif bin_bid_offset > okx_ask_offset:
-                gap_price = round((bin_bid_offset - okx_ask_offset *100 / price), 2)
-                self.logger.warning(f'Buy {symbol} on Okx, Sell {symbol} on Binance, gap: {gap_price}')
+            elif negative_gap > offset:
+                okx_ask = book['okx_ask']
+                bin_bid = book['bin_bid']
+                self.logger.warning(f'Buy {symbol} on Okx at {okx_ask}, Sell {symbol} on Binance at {bin_bid}')
                 okx_ask_size = book['okx_ask_size']
                 bin_bid_size = book['bin_bid_size']
                 trading_lot = min(okx_ask_size, bin_bid_size)
